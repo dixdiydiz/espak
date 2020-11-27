e6fbf82ed94689e14323371ed59bb9543b9ac60a
邓瑞源 <dry@dry-mbp.local>
2020-11-27 09:48:17 +0800

    feat: draft

--- dist/node/config.d.ts
+++
@@ -1,6 +0,0 @@
-export interface UserConfig {
-    public?: string;
-    entry?: string | Record<string, string>;
-    output?: string;
-}
-export declare function generateConfig(): Promise<void>;
--- dist/node/constant.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const DEVELOPMENT = 1;
-export declare const PRODUCTION = 2;
