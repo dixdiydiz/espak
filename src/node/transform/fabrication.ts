a202177c899ab75f3e23efe6db79e12f8c1a8238
dry <diyc@foxmail.com>
2020-12-08 17:53:01 +0800

    feat: draft

--- src/node/transform/fabrication.ts
+++ src/node/transform/fabrication.ts
@@ -1,4 +1,5 @@
-// import { builtinModules } from 'module'
+import { builtinModules } from 'module'
+const { dependencies } = require('../../../package.json')
 import resolve from 'resolve'
 import log from 'loglevel'
 
@@ -9,11 +10,19 @@ interface RawInModule {
   label?: string
   text: string
 }
+interface EegaModule extends RawInModule {}
+
+type ModuleType = 'builtin' | 'third' | 'custom'
+
+export const srcToBuild = Object.create(null)
 
 const importedReg = /(?:import\((?:(?:'(\S+)')|(?:"(\S+)"))\))|(?:import\s*(?:(?:'(\S+)')|(?:"(\S+)"));)|(?:import\s+[^;\"\']+from\s*(?:(?:'([^\s\']+)')|(?:"([^\s\"]+)"));)/g
 
 export async function handleImportation(input: RawInModule): Promise<any> {
-  const { dir, text } = input
+  const { infile, dir, text } = input
+  srcToBuild[infile] = {
+    ...input,
+  }
   const matchs = text.matchAll(importedReg)
   for (let m of matchs) {
     // const match = {
@@ -33,9 +42,26 @@ export async function handleImportation(input: RawInModule): Promise<any> {
           ...options,
         })
         console.log(absolutePath)
+        return {
+          infile: absolutePath,
+          fromPath: relativePath,
+          moduleType: obtainModuleType(relativePath),
+        }
       }
     } catch (e) {
       log.warn(e)
+      return {
+        infile: '',
+        fromPath: '',
+      }
     }
+  }
+  function obtainModuleType(fromPath: string): ModuleType {
+    if (builtinModules.includes(fromPath)) {
+      return 'builtin'
+    } else if (Object.keys(dependencies).includes(fromPath)) {
+      return 'third'
+    }
+    return 'custom'
   }
 }
